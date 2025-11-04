71f010608ad9d46ff88313b88e98993e57f618e5
Muhammad Zafar <engr.awaiszafar@gmail.com>
2025-11-04 04:37:44 -0600

    Add API tests for core app

--- core/views.py
+++ core/views.py
@@ -4,7 +4,6 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from datetime import date
-from .models import WorkoutSet, DietLog, FoodItem
 from .serializers import WorkoutSetSerializer
 
 # --- UPDATED IMPORTS ---
